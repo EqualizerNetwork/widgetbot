e5cbf8c9a35ab450b4b6e7ad99951ec2130ac68e
Arnaud Lier <zeprofdecoding@gmail.com>
2019-11-27 13:51:08 +0100

    Create fr.ts

---
+++ packages/embed/src/app/locales/translations/fr.ts
@@ -0,0 +1,7 @@
+const translation = {
+  'message.join_message': `Hé ! Écoute ! {name} nous a rejoint !`,
+  'message.channel': `Envoyer un message à {channel}`,
+  'header.join': `Rejoindre`
+}
+
+export default translation
