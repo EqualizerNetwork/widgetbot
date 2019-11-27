3fa353892fd51728e99304637f29a0f1fc3af7e2
Arnaud Lier <zeprofdecoding@gmail.com>
2019-11-27 13:29:18 +0100

    Update index.tsx

--- packages/embed/src/components/Channels/Panel/index.tsx
+++ packages/embed/src/components/Channels/Panel/index.tsx
@@ -1,7 +1,7 @@
 import { connect } from 'fluent'
 import * as React from 'react'
 
-import { Root, Version } from './elements'
+import { Root } from './elements'
 
 const { version } = require('../../../../package.json')
 
@@ -12,37 +12,10 @@ export default connect()
   .toClass(
     props =>
       class Panel extends React.PureComponent<typeof props> {
-        toggleDev(name: string) {
-          const { toggle } = this.props
-
-          toggle({
-            open: true,
-            type: 'developer',
-            data: name
-          })
-        }
-
-        toggleAbout(event: Event) {
-          const { toggle } = this.props
-          event.preventDefault()
-
-          toggle({
-            open: true,
-            type: 'about',
-            data: null
-          })
-        }
 
         render() {
           return (
             <Root>
-              <Version
-                href={`https://github.com/widgetbot-io/embed`}
-                target="_blank"
-                onClick={this.toggleAbout.bind(this)}
-              >
-                {`v${version}`}
-              </Version>
             </Root>
           )
         }
